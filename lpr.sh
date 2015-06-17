6fe3a9c76175d93b50f38d265a16e0786d859b43
Ioannis Konstadelias <ikonstadel@gmail.com>
2015-06-17 16:09:10 +0300

    Iterate through letter images in a faster way.

--- lpr.sh
+++ lpr.sh
@@ -12,13 +12,13 @@ fi
 rm -f output.txt
 ./1_text_isolation/txtiso.out $1 > /dev/null 2>&1
 ./2_character_segmentation/charsegm.out final.png > /dev/null 2>&1
-for i in ` ls | grep letter `;
+for i in letter*.png;
 do
     ./3_ocr/ocr.out $i | grep '[Α-Ωα-ω0-9]' | perl -p -e 's/[^Α-Ω0-9]+//' | tr '\n' '\0' >> output.txt
 done
 
 rm final.png
-for i in ` ls | grep letter `;
+for i in letter*.png;
 do
     rm $i
 done
