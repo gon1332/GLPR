66ea15522b82dc6ab65fa8e337a38b17cd3e7fe8
Ioannis Konstadelias <ikonstadel@gmail.com>
2015-06-17 16:52:17 +0300

    Eliminate the call to tr.

--- lpr.sh
+++ lpr.sh
@@ -14,7 +14,7 @@ rm -f output.txt
 ./2_character_segmentation/charsegm.out final.png > /dev/null 2>&1
 for i in letter*.png;
 do
-    ./3_ocr/ocr.out $i | grep '[Α-Ωα-ω0-9]' | perl -p -e 's/[^Α-Ω0-9]+//' | tr '\n' '\0' >> output.txt
+    ./3_ocr/ocr.out $i | grep '[Α-Ωα-ω0-9]' | perl -p -e 's/[^Α-Ω0-9]+//g' >> output.txt
 done
 
 rm final.png
