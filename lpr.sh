f8f62817cf9b24d9bc878b408e79e1560c291f9a
Ioannis Konstadelias <ikonstadel@gmail.com>
2015-06-17 17:28:10 +0300

    Merge the two for-loops.

--- lpr.sh
+++ lpr.sh
@@ -2,6 +2,7 @@
 
 EXPECTED_ARGS=1
 E_BADARGS=1
+SUCCESS=0
 
 if [ $# -ne $EXPECTED_ARGS ]
 then
@@ -12,13 +13,11 @@ fi
 rm -f output.txt
 ./1_text_isolation/txtiso.out $1 > /dev/null 2>&1
 ./2_character_segmentation/charsegm.out final.png > /dev/null 2>&1
-for i in letter*.png;
-do
-    ./3_ocr/ocr.out $i | grep '[Α-Ωα-ω0-9]' | perl -p -e 's/[^Α-Ω0-9]+//g' >> output.txt
-done
-
 rm final.png
 for i in letter*.png;
 do
+    ./3_ocr/ocr.out $i | grep '[Α-Ωα-ω0-9]' | perl -p -e 's/[^Α-Ω0-9]+//g' >> output.txt
     rm $i
 done
+
+exit $SUCCESS
