47712ce0022ef4cfb464a8dcc80df578783939dc
Ioannis Konstadelias <ikonstadel@gmail.com>
2015-06-25 21:22:03 +0300

    Change the arguments that ocr.out accepts.
    Remove the number of images. It's redundant.

--- 3_ocr/main.cpp
+++ 3_ocr/main.cpp
@@ -6,25 +6,13 @@
 
 int main(int argc, char *argv[])
 {
-    if (argc < 2) {
-        std::cerr << "Usage: " << argv[0] << "<N> [images]" << std::endl;
+    if (argc == 1) {
+        std::cerr << "No images to work on." << std::endl;
         return 1;
     }
 
-    int imgs = atoi(argv[1]);
-
-    if (imgs == 0) {
-        std::cerr << "No images to work on." << std::endl;
-        return 0;
-    }
-
-    if (imgs > 10) {
-        std::cerr << "Number of images limit exceeded." << std::endl;
-        return 0;
-    }
-
-    tesseract::TessBaseAPI *api = new tesseract::TessBaseAPI();
     // Initialize tesseract-ocr with English, without specifying tessdata path
+    tesseract::TessBaseAPI *api = new tesseract::TessBaseAPI();
     if (api->Init(NULL, "ell")) {
         fprintf(stderr, "Could not initialize tesseract.\n");
         exit(1);
@@ -33,8 +21,8 @@ int main(int argc, char *argv[])
     // Open input images with leptonica library and store them in a vector
     std::vector<Pix *> letter_imgs;
 
-    int args_offset = 2;
-    for (int i = 0; i < imgs; ++i)
+    int args_offset = 1;
+    for (int i = 0; i < argc-1; ++i)
         letter_imgs.push_back( pixRead( argv[ args_offset + i ] ) );
 
     std::ofstream output;
--- lpr.sh
+++ lpr.sh
@@ -14,12 +14,15 @@ rm -f output.txt
 ./1_text_isolation/txtiso.out $1 > /dev/null 2>&1
 ./2_character_segmentation/charsegm.out final.png > /dev/null 2>&1
 rm final.png
-./3_ocr/ocr.out 7 letter*.png
+./3_ocr/ocr.out letter*.png
+rm letter*.png
 
 echo "-------------------------------"
 echo "Greek License Plate: "
 cat output.txt | grep '[Α-Ω0-9]' | perl -p -e 's/[^Α-Ω0-9]+//g'
 printf "\n"
 echo "-------------------------------"
+
+rm output.txt
 
 exit $SUCCESS
